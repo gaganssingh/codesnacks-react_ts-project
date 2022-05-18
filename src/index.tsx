185a5eeafd776fbb5b707c56ee6321495240bdfc
Gagan Singh <gagansingh.dev@outlook.com>
2022-05-18 12:48:09 -0400

    Implemented basic transpiling using esbuild-wasm

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,31 @@
-import { useState } from "react";
+import * as esbuild from "esbuild-wasm";
+import { useEffect, useRef, useState } from "react";
 import { createRoot } from "react-dom/client";
 
 const App: React.FC = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
-  const onClick = () => {
-    setCode(input);
+  // Init ESBuild
+  const startService = async () => {
+    await esbuild.initialize({
+      worker: true,
+      wasmURL: "https://unpkg.com/esbuild-wasm@0.14.39/esbuild.wasm",
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    const result = await esbuild.transform(input, {
+      loader: "jsx",
+      target: "es2015",
+    });
+
+    setCode(result.code);
   };
 
   return (
